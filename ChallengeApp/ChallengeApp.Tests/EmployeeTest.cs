2aac87c0441e5f16e9743c02db5bf9d6f1110d31
Piotr <piotrax@gmail.com>
2023-11-03 18:26:19 +0100

    zadanie dzień 9

--- ChallengeApp/ChallengeApp.Tests/EmployeeTest.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTest.cs
@@ -5,21 +5,24 @@ namespace ChallengeApp.Tests
     public class Tests
     {   
         [Test]
-        public void CollectFiveGrades_ShouldReturnCorrectResultSum()
+        public void Employee_tests_Min_Max_Average()
         {
             // arrange
             var employee = new Employee("Pawe³", "Paw³owski");
             employee.AddGrade(0x5);
             employee.AddGrade(0x7);
-            employee.AddGrade(-0x7);
+            employee.AddGrade(-0x5);
             employee.AddGrade(-0xa);
             employee.AddGrade(0b111);
 
             // act
             var result = employee.GetGrade();
+            var stats = employee.GetStatistics();
 
             //assert
-            Assert.AreEqual(2, result);
+            Assert.AreEqual(7, stats.Max);
+            Assert.Greater(11, stats.Min);
+            Assert.AreEqual(0, stats.Average);
         }
     }
 }
